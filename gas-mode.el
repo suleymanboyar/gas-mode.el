13dd6c5278ccf3402baf6ea5f870524e0ecce466
Enoc <brianhernandez222@hotmail.com>
2021-07-28 22:22:41 -0600

    Remove unused variables

--- gas-mode.el
+++ gas-mode.el
@@ -1,4 +1,4 @@
-;;; minimal-gas-mode.el --- Mode for editing assembly code with AT&T sintax -*- lexical-biding: t -*-
+;;; gas-mode.el --- Mode for editing assembly code with AT&T sintax -*- lexical-biding: t -*-
 
 ;; Copyright (C) 2021 Bryan Hernández
 
@@ -91,10 +91,6 @@ relies on `gas-initial-indent'to set the indentation needed.")
     "sysexit" "rdtsc" "int" "leave")
   "Instructions used in assembly programming")
 
-(defconst gas-instructions-regex
-  (regexp-opt gas-instructions)
-  "Regex that matches all the elements in `gas-instructions'")
-
 (defconst gas-pseudo-ops
   '(".abort" ".align" ".altmacro" ".ascii" ".asciz"
     ".attach_to_group" ".balign" ".bundle_align_mode"
@@ -122,25 +118,9 @@ relies on `gas-initial-indent'to set the indentation needed.")
     ".8byte" ".bss" ".rodata")
   "Assembler directives (a.k.a Pseudo operators) used by Gas")
 
-(defconst gas-registers
-  '("%rax" "%eax" "%ax" "%ah" "%al" "%rcx" "%ecx"
-    "%cx" "%cx" "%ch" "%cl" "%rdx" "%edx" "%dx"
-    "%dh" "%dl" "%rbx" "%ebx" "%bx" "%bh" "%bl"
-    "%rsp" "%esp" "%sp" "%spl" "%rbp" "%ebp"
-    "%bp" "%bpl" "%rsi" "%esi" "%si" "%sil"
-    "%rdi" "%edi" "%di" "%dil" "%eflags" "%rip"
-    "%r8" "%r9" "%r10" "%r11" "%r12" "%r13"
-    "%r14" "%r15" "%xmm0" "%xmm1" "%xmm2"
-    "%xmm3" "%xmm4" "%xmm5" "%xmm6" "%xmm7"
-    "%xmm8" "%xmm9" "%xmm10" "%xmm11" "%xmm12"
-    "%xmm12" "%xmm13" "%xmm14" "%xmm15" "%ymm0"
-    "%ymm1" "%ymm2" "%ymm3" "%ymm4" "%ymm5" "%ymm6"
-    "%ymm7" "%ymm8" "%ymm9" "%ymm10" "%ymm11"
-    "%ymm12" "%ymm13" "%ymm14" "%ymm15" "%zmm0"
-    "%zmm1" "%zmm2" "%zmm3" "%zmm4" "%zmm5" "%zmm6"
-    "%zmm7" "%zmm8" "%zmm9" "%zmm10" "%zmm11"
-    "%zmm12" "%zmm13" "%zmm14" "%zmm15")
-  "Register available for assembly programming (at least in x86 and x86_64 registerl")
+(defconst gas-instructions-regex
+  (regexp-opt gas-instructions)
+  "Regex that matches all the elements in `gas-instructions'")
 
 (defconst gas-mode-font-lock
   `((,(regexp-opt gas-pseudo-ops) . font-lock-builtin-face)
